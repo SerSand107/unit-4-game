27b559130be0fe9970f294cc5a9db1c3d2cc8cbf
Leon IV <38409992+SerSand107@users.noreply.github.com>
2018-08-27 18:07:37 -0500

    working progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,21 @@
-$(document).ready(function){
-    
-}
+$(document).ready(function() {
+    $("#comp-number").html(Math.floor((Math.random()* 120) + 1));
+
+    $("#blue").on("click", function() {
+        $(".total-score").html(Math.floor((Math.random()*12) + 1));
+    });
+
+    $("#green").on("click", function() {
+        $("#green").html(Math.floor((Math.random()*12) + 1));
+    });
+
+    $("#purple").on("click", function() {
+        $("#purple").html(Math.floor((Math.random()*12) + 1));
+    });
+
+    $("#red").on("click", function() {
+        $("#user-number").html(Math.floor((Math.random()*12) + 1));
+    });
+
+    $("#wins")
+});
